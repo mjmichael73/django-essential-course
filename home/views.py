d2c88e98bf61838fe3df1c355936c039c5f2eb09
Mojtaba Michael <mojimich2015@gmail.com>
2022-09-13 00:36:47 +0300

    Added signup page and navbar

--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = "home/register.html"
+    success_url = "/smart/notes/"
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 
 class LoginInterfaceView(LoginView):
