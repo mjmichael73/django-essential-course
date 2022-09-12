8eeaf8accfd922b7cfa77039a6845eb83b4e3831
Mojtaba Michael <mojimich2015@gmail.com>
2022-09-12 19:38:24 +0300

    Changed function based views to Class Based Views for notes app

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
-    path('notes/<int:pk>', views.detail),
+    path('notes/', views.NotesListView.as_view()),
+    path('notes/<int:pk>', views.NotesDetailView.as_view()),
 ]
